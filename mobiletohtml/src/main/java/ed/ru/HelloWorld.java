3c220e52d310edc5ea7f0c1fa107da735bb523d9
ruya <ruya>
2016-02-09 15:12:52 +0200

    hash

--- mobiletohtml/src/main/java/ed/ru/HelloWorld.java
+++ mobiletohtml/src/main/java/ed/ru/HelloWorld.java
@@ -10,25 +10,23 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 public class HelloWorld {
-	
+
 	public static void main(String[] args) {
 		get("/hello", (request, response) -> {
 			Map<String, Object> model = new HashMap<>();
 			model.put("type", "text");
-			
-			Map<String, String> attributes = new LinkedHashMap<String, String>();
-			
-			attributes.put("name","person_input");
-			attributes.put("value","Heisenberg");
-			attributes.put("onclick","alert('tada')");
-			
-			model.put("attributes", attributes);
-			
-//			model.put("attr", "onclick")
 
-				return new ModelAndView(model, "/input/input.vm");
-			}, new VelocityTemplateEngine());
- 
+			Map<String, String> attributes = new LinkedHashMap<String, String>();
+
+			attributes.put("name", "person_input");
+			attributes.put("value", "Heisenberg");
+			attributes.put("onclick", "alert('tada')");
+
+			model.put("attributes", attributes);
+
+			return new ModelAndView(model, "/input/input.vm");
+		}, new VelocityTemplateEngine());
+
 	}
 
 }
